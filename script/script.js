c274d4abc3ac221a95e7296e0c379a099291b172
hpt1mee <n.kurlandchick@gmail.com>
2024-12-12 23:33:24 +0300

    Adding smth

--- script/script.js
+++ script/script.js
@@ -1,25 +1,4 @@
 const dops = document.querySelector('.Dops')
-const scrollWrap = document.querySelector('body')
-
-const height = () => scrollWrap.getBoundingClientRect().height
-const speed = .08
-
-let offset = 0
-
-// Scroll
-const smoothScroll = () => {
-    offset += (window.pageYOffset - offset) * speed
-
-    scrollWrap.style.transform = 'translateY(-' + offset + 'px) translateZ(0)'
-
-    window.requestAnimationFrame(smoothScroll)
-}
-smoothScroll()
-
-// Resize
-const handleResize = () => {
-    document.body.style.height = Math.floor(height()) + 'px'
-}
-handleResize()
-
-window.addEventListener('resize', handleResize)
+dops.addEventListener('click',() => {
+	window.location.href = '../html/dops.html'
+})
